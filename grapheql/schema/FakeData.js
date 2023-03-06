a3367699d6f765d740037e12dea9a6ea623f6e7b
bakiriabdelmalek <bakiriabdelmalek1@gmail.com>
2023-03-06 20:30:26 +0100

    graphe ql example

---
+++ grapheql/schema/FakeData.js
@@ -0,0 +1,43 @@
+const UserList = [
+    {
+        id:1,
+        name:"John",
+        username:"John",
+        age:20,
+        nationality:"Canada"
+
+    },
+    {
+        id:2,
+        name:"pedro",
+        username:"Johnamogus",
+        age:22,
+        nationality:"brazil"
+
+    },
+    {
+        id:3,
+        name:"amigo",
+        username:"jandi",
+        age:30,
+        nationality:"Cameron"
+
+    },
+    {
+        id:4,
+        name:"cg",
+        username:"digi",
+        age:18,
+        nationality:"estonia"
+
+    },
+    {
+        id:5,
+        name:"miol",
+        username:"barikho",
+        age:46,
+        nationality:"Mexico"
+
+    }
+]
+module.exports = {UserList};
